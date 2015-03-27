6da1c92880d9ced9ee36edaee253281ebcc06c7e
Vallium <aalliot@student.42.fr>
2015-03-27 18:34:31 +0100

    print.c

---
+++ src/print.c
@@ -0,0 +1,18 @@
+#include "push_swap.h"
+
+void		print_move(t_stacks *st, char *str, char *color)
+{
+	if (st->nb_act && !(st->options & 8))
+	{
+		ft_putchar(' ');
+	}
+	if (st->options & 2)
+		ft_putstr(color);
+	ft_putstr(str);
+	ft_putstr("\033[0m");
+	if (st->options & 8)
+	{
+		ft_putchar('\n');
+		print_stacks(st);
+	}
+}
