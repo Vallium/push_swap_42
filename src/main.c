7ce3ea57b400edbf433c4f4b7935f70446bc0116
Vallium <aalliot@student.42.fr>
2015-03-28 23:11:07 +0100

    bizarre

--- src/main.c
+++ src/main.c
@@ -84,7 +84,7 @@ int		main(int argc, char **argv)
 
 	if (argc > 1)
 		all.options = get_opt_assi(argc, argv, &opt);
-	if (!error_handling(argv + opt.nb - 1))
+	if (argc == 1 || !error_handling(argv + opt.nb - 1))
 		return (0);
 	fill_stacks(&all, argv + opt.nb - 1, argc - opt.nb + 1);
 	check_dups(all);
